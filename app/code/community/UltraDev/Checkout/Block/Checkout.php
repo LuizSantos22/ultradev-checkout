04206b62a3b9e9cb27792943e303425bddf3f450
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-07-06 01:53:08 -0300

    Update Checkout.php

--- app/code/community/UltraDev/Checkout/Block/Checkout.php
+++ app/code/community/UltraDev/Checkout/Block/Checkout.php
@@ -8,14 +8,11 @@ class UltraDev_Checkout_Block_Checkout extends Mage_Core_Block_Template
     {
         $items = [];
         $quote = Mage::getSingleton('checkout/session')->getQuote();
-
         foreach ($quote->getAllVisibleItems() as $item) {
-            /** @var Mage_Sales_Model_Quote_Item $item */
             $product  = $item->getProduct();
             $imageUrl = Mage::helper('catalog/image')->init($product, 'thumbnail')
                             ->resize(68, 68)
                             ->__toString();
-
             $options = [];
             if ($item->getProductType() === 'configurable') {
                 foreach ($item->getProduct()->getTypeInstance(true)
@@ -25,7 +22,6 @@ class UltraDev_Checkout_Block_Checkout extends Mage_Core_Block_Template
                     }
                 }
             }
-
             $items[] = [
                 'item_id'   => $item->getId(),
                 'name'      => $item->getName(),
@@ -36,8 +32,61 @@ class UltraDev_Checkout_Block_Checkout extends Mage_Core_Block_Template
                 'options'   => implode(', ', $options),
             ];
         }
-
         return $items;
+    }
+
+    /**
+     * Retorna os métodos de pagamento ativos do OpenMage
+     */
+    public function getPaymentMethods()
+    {
+        $quote   = Mage::getSingleton('checkout/session')->getQuote();
+        $store   = $quote->getStoreId();
+        $methods = [];
+        try {
+            $availableMethods = Mage::helper('payment')->getStoreMethods($store, $quote);
+        } catch (Exception $e) {
+            return $methods;
+        }
+        foreach ($availableMethods as $method) {
+            if (!$method->canUseCheckout()) {
+                continue;
+            }
+            try {
+                $block = Mage::getBlockSingleton($method->getFormBlockType());
+                if ($block) {
+                    $block->setMethod($method)->setQuote($quote);
+                    $html = $block->toHtml();
+                } else {
+                    $html = '';
+                }
+            } catch (Exception $e) {
+                $html = '';
+            }
+            $methods[] = [
+                'code'  => $method->getCode(),
+                'title' => (string) $method->getTitle(),
+                'block' => $html,
+            ];
+        }
+        return $methods;
+    }
+
+    /**
+     * Retorna todas as regiões agrupadas por país para o JS
+     */
+    protected function _getAllRegions()
+    {
+        $collection = Mage::getModel('directory/region')->getCollection()->load();
+        $regions = [];
+        foreach ($collection as $region) {
+            $regions[$region->getCountryId()][] = [
+                'id'   => $region->getId(),
+                'code' => $region->getCode(),
+                'name' => $region->getName(),
+            ];
+        }
+        return $regions;
     }
 
     /**
@@ -45,23 +94,71 @@ class UltraDev_Checkout_Block_Checkout extends Mage_Core_Block_Template
      */
     public function getCheckoutConfig()
     {
-        $helper = Mage::helper('ultradev_checkout');
-        $quote  = Mage::getSingleton('checkout/session')->getQuote();
+        $helper   = Mage::helper('ultradev_checkout');
+        $quote    = Mage::getSingleton('checkout/session')->getQuote();
+        $session  = Mage::getSingleton('customer/session');
+        $customer = null;
+
+        if ($session->isLoggedIn()) {
+            $c       = Mage::getModel('customer/customer')->load($session->getCustomerId());
+            $address = $c->getDefaultBillingAddress();
+
+            $streetLines = $address ? $address->getStreet() : [];
+            $regionCode  = '';
+            if ($address && $address->getRegionId()) {
+                $region     = Mage::getModel('directory/region')->load($address->getRegionId());
+                $regionCode = $region->getCode();
+            }
+
+            $dobFormatted = '';
+            if ($c->getDob()) {
+                $dobFormatted = date('d/m/Y', strtotime($c->getDob()));
+            }
+
+            $customer = [
+                'firstname'          => $c->getFirstname(),
+                'lastname'           => $c->getLastname(),
+                'email'              => $c->getEmail(),
+                'taxvat'             => $c->getTaxvat(),
+                'dob'                => $dobFormatted,
+                'telephone'          => $address ? $address->getTelephone() : '',
+                'postcode'           => $address ? preg_replace('/\D/', '', $address->getPostcode()) : '',
+                'street'             => isset($streetLines[0]) ? $streetLines[0] : '',
+                'number'             => isset($streetLines[1]) ? $streetLines[1] : '',
+                'complement'         => isset($streetLines[2]) ? $streetLines[2] : '',
+                'district'           => isset($streetLines[3]) ? $streetLines[3] : '',
+                'city'               => $address ? $address->getCity() : '',
+                'region_id'          => $regionCode,
+                'tipopessoa'         => $c->getData('tipopessoa'),
+                'cnpj'               => $c->getData('cnpj'),
+                'razao_social'       => $c->getData('razao_social'),
+                'cpf_responsavel'    => $c->getData('cpf_responsavel'),
+                'inscricao_estadual' => $c->getData('inscricao_estadual'),
+            ];
+        }
 
         return [
-            'ajaxUrl'         => Mage::getUrl('ultradev-checkout/index/placeOrder'),
-            'shippingUrl'     => Mage::getUrl('ultradev-checkout/index/shipping'),
-            'installmentsUrl' => Mage::getUrl('ultradev-checkout/index/installments'),
-            'pixDiscount'     => $helper->getPixDiscount(),
-            'maxInstallments' => $helper->getMaxInstallments(),
-            'freeInstallments'=> $helper->getFreeInstallments(),
-            'subtotal'        => (float) $quote->getSubtotal(),
-            'formKey'         => Mage::getSingleton('core/session')->getFormKey(),
+            'ajaxUrl'          => Mage::getUrl('ultra-checkout/index/placeOrder'),
+            'shippingUrl'      => Mage::getUrl('ultra-checkout/index/shipping'),
+            'installmentsUrl'  => Mage::getUrl('ultra-checkout/index/installments'),
+            'loginUrl'         => Mage::getUrl('ultra-checkout/index/login'),
+            'pixDiscount'      => $helper->getPixDiscount(),
+            'maxInstallments'  => $helper->getMaxInstallments(),
+            'freeInstallments' => $helper->getFreeInstallments(),
+            'subtotal'         => (float) $quote->getSubtotal(),
+            'formKey'          => Mage::getSingleton('core/session')->getFormKey(),
+            'regions'          => $this->_getAllRegions(),
+            'customer'         => $customer,
         ];
     }
 
-    public function getCheckoutConfigJson()
+   public function getCheckoutConfigJson()
     {
         return json_encode($this->getCheckoutConfig());
+    }
+
+    public function isCustomerLoggedIn()
+    {
+        return Mage::getSingleton('customer/session')->isLoggedIn();
     }
 }
